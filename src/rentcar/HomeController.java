d1e72c3d9174eea4b624a6b4233014c5e05b445a
vietdung96 <102815340+vietdung96@users.noreply.github.com>
2022-10-29 20:56:08 +0700

    first commit

--- src/rentcar/HomeController.java
+++ src/rentcar/HomeController.java
@@ -1,4 +1,15 @@
 package rentcar;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 public class HomeController {
+    public void goToBookList(ActionEvent actionEvent) throws Exception {
+        Parent listBook = FXMLLoader.load(getClass().getResource("customer/list/list.fxml"));
+        Main.rootStage.setTitle("Customer");
+        Main.rootStage.setScene(new Scene(listBook,800,600));
+    }
+
 }
